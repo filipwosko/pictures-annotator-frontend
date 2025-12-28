5d893b577abe6d40c5b261a0626fe785b8881200
Filip Wośko <filipwosko96@gmail.com>
2025-12-28 18:38:53 +0100

    dodanie swojego logo

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
   <div>
     <nav class="navbar navbar-expand navbar-dark bg-dark px-3">
-      <a class="navbar-brand">Pictures Annotator</a>
+      <router-link class="navbar-brand d-flex align-items-center" to="/">
+        <img
+          src="@/assets/logo.png"
+          alt="Logo"
+          height="30"
+          class="me-2"
+        />
+        Pictures Annotator
+      </router-link>
 
       <div class="navbar-nav">
         <router-link class="nav-link" to="/pictures">Lista zdjęć</router-link>
